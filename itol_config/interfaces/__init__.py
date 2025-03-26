69b40e1a17fde41cc1a632c9e644789f9678b928
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-26 07:00:52 +0000

    Add range label

--- itol_config/interfaces/__init__.py
+++ itol_config/interfaces/__init__.py
@@ -1,3 +1,4 @@
 from .colour_strip import ColourStripConfigWriter
 from .text_label import TextLabelConfigWriter
 from .binary_data import BinaryDataConfigWriter
+from .ranges import RangeConfigWriter
